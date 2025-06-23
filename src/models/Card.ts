ca1350f04dba393581212193150cc75195574a9b
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-22 22:39:36 -0300

    fix: correct typo in items property name

--- src/models/Card.ts
+++ src/models/Card.ts
@@ -2,7 +2,7 @@ import { BaseModel } from "./BaseModel"
 import { Item } from "./Item"
 
 export class Card extends BaseModel {
-  private _itens: Map<Item["id"], Item> = new Map()
+  private _items: Map<Item["id"], Item> = new Map()
   private _description: string
 
   public constructor(description: string) {
@@ -12,14 +12,14 @@ export class Card extends BaseModel {
   }
 
   public addItem(item: Item) {
-    this._itens.set(item.id, item)
+    this._items.set(item.id, item)
   }
 
   public removeItem(id: string) {
-    this._itens.delete(id)
+    this._items.delete(id)
   }
 
-  public get itens() { return this._itens }
+  public get items() { return this._items }
   public get description() { return this._description }
 
   public set description(value: string)  { this._description = value }
