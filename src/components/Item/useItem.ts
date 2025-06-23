f036b98d2cd7294ee435b8028ad4068a3bf687ef
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-22 22:09:10 -0300

    feat: add timer context and calculate time difference

--- src/components/Item/useItem.ts
+++ src/components/Item/useItem.ts
@@ -1,19 +1,25 @@
-import { useContext } from "react";
+import { useCallback, useContext, useMemo } from "react";
 import { CardsContext } from "../../contexts/cards";
+import { TimerContext } from "../../contexts/timer";
 import { Item } from "../../models";
 import { EventEmitter } from "../../Utils/EventEmitter";
+import { DateDiff } from "../../Utils/DateDiff";
 
 export function useItem(item: Item) {
   const { cards } = useContext(CardsContext)
+  const { timer } = useContext(TimerContext)
 
-  const removeItem = () => {
+  const removeItem = useCallback(() => {
     const card = cards.get(item.cardId)!
 
     card.removeItem(item.id)
     EventEmitter.dispatch(card.id, { quantity: card.itens.size })
-  }
+  }, [cards, item])
+
+  const timerDiff = useMemo(() => DateDiff(timer, item.createdAt), [item.createdAt, timer])
 
   return {
-    removeItem
+    removeItem,
+    timerDiff
   }
 }
