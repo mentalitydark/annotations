f363bb85963dbddd76f051aa81c0a9c9b3273587
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-23 22:35:40 -0300

    fix: wrap CardElements in a div for structure

--- src/components/NewItem/NewItem.tsx
+++ src/components/NewItem/NewItem.tsx
@@ -6,31 +6,33 @@ export function NewItem() {
 const { createItemSubmit, itemInputRef, cardSelectRef, cards } = useNewItem()
 
   return (
-    <CardElements.Main>
-      <CardElements.Header>
-        <CardElements.Title value="Novo Item" />
-      </CardElements.Header>
-      <CardElements.Content>
-        <select ref={cardSelectRef} name="card-item-select" id="card-item-select" defaultValue="" className="select">
-          <option value="" disabled>Selecione um card</option>
-          {
-            Array
-              .from(cards)
-              .map(([key, card]) => <option key={key} value={card.id}>{card.description}</option>)
-          }
-        </select>
-        <input
-          ref={itemInputRef}
-          type="text"
-          name="new-input-input"
-          className="input-text"
-          placeholder="Descrição do item"
-          onKeyDown={EnterEvent(createItemSubmit)}
-          />
-      </CardElements.Content>
-      <CardElements.Footer>
-        <CardElements.Action title="ADICIONAR" action={createItemSubmit} />
-      </CardElements.Footer>
-    </CardElements.Main>
+    <div className="new-item">
+      <CardElements.Main>
+        <CardElements.Header>
+          <CardElements.Title value="Novo Item" />
+        </CardElements.Header>
+        <CardElements.Content>
+          <select ref={cardSelectRef} name="card-item-select" id="card-item-select" defaultValue="" className="select">
+            <option value="" disabled>Selecione um card</option>
+            {
+              Array
+                .from(cards)
+                .map(([key, card]) => <option key={key} value={card.id}>{card.description}</option>)
+            }
+          </select>
+          <input
+            ref={itemInputRef}
+            type="text"
+            name="new-input-input"
+            className="input-text"
+            placeholder="Descrição do item"
+            onKeyDown={EnterEvent(createItemSubmit)}
+            />
+        </CardElements.Content>
+        <CardElements.Footer>
+          <CardElements.Action title="ADICIONAR" action={createItemSubmit} />
+        </CardElements.Footer>
+      </CardElements.Main>
+    </div>
   )
 }
