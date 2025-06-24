6f039adfb249df229d51da248bf2a82a8a62dd94
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-23 21:45:10 -0300

    fix: remove TimerContext and update timerDiff

--- src/components/Item/useItem.ts
+++ src/components/Item/useItem.ts
@@ -1,13 +1,11 @@
 import { useCallback, useContext, useMemo } from "react";
 import { CardsContext } from "../../contexts/cards";
-import { TimerContext } from "../../contexts/timer";
 import { Item } from "../../models";
 import { EventEmitter } from "../../Utils/EventEmitter";
 import { DateDiff } from "../../Utils/DateDiff";
 
 export function useItem(item: Item) {
   const { cards } = useContext(CardsContext)
-  const { timer } = useContext(TimerContext)
 
   const removeItem = useCallback(() => {
     const card = cards.get(item.cardId)!
@@ -16,7 +14,7 @@ export function useItem(item: Item) {
     EventEmitter.dispatch(card.id, { quantity: card.items.size })
   }, [cards, item])
 
-  const timerDiff = useMemo(() => DateDiff(timer, item.createdAt), [item.createdAt, timer])
+  const timerDiff = useMemo(() => DateDiff(item.timerUsed, item.createdAt), [item])
 
   return {
     removeItem,
