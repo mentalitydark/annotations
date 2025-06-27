12145398c52b618e7337a1850dcfc3ff576feae2
Guilherme Oliveira <guilherme.oliveira.01@outlook.com>
2025-06-26 21:16:48 -0300

    feat: integrate Toast notifications in card management

--- src/components/NewCard/useNewCard.ts
+++ src/components/NewCard/useNewCard.ts
@@ -1,9 +1,11 @@
 import { useContext, useRef } from 'react'
 import { CardsContext } from '../../contexts/cards'
 import { InvalidArgument } from '../../Errors'
+import { ToastContext } from '../../contexts/toast'
 
 export function useNewCard() {
   const { createCard } = useContext(CardsContext)
+  const { success, error } = useContext(ToastContext)
   const inputRef = useRef<HTMLInputElement>(null)
 
   const createCardSubmit = () => {
@@ -16,9 +18,10 @@ export function useNewCard() {
 
       inputRef.current.value = ""
       inputRef.current.focus()
+      success('Card criado')
     } catch (e) {
       if (e instanceof InvalidArgument) {
-        console.log(e.message)
+        error(e.message)
       }
     }
   }
